17683469c06e8efa8593094d466de573a16fc355
5huv0 <md88748874@gmail.com>
2025-03-18 07:16:31 +0600

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+// import { useState } from 'react'
+// import reactLogo from './assets/react.svg'
+// import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+    <div className='bg-green-600 font-mono text-50xl'>
+        <h1>hi there</h1>
+    </div>
+    </>
+  )
+}
+
+export default App
