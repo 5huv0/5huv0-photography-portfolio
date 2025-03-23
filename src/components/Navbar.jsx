7a6f0fd78d6d6e736939cc075e0e56ac1668a0c7
5huv0 <md88748874@gmail.com>
2025-03-23 23:24:27 +0600

    navbar almost done fot now

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,23 +1,25 @@
 import React from 'react'
+// import { motion } from "motion/react"
 import { NavLink, Link} from 'react-router-dom'
 
 export default function Navbar() {
   return (
-    <div className='bg-yellow-300'>
+    <div className='nav font-mono p-3'>
       <nav className='flex justify-between'>
 
         <div className=''> {/* This div is for site logo */}
           <Link to='/'>
-            <img src="../../public/images/logo-1.png" alt="" className='size-20 rounded-full'/>
+            <img src="../../public/images/logo-1.png" alt="" className='size-18 rounded-full'/>
           </Link>
         </div>
 
-        <div className='flex gap-10'>
-          <NavLink to="">o</NavLink>
-          <NavLink to="">p</NavLink>
-          <NavLink to="">w</NavLink>
-          <NavLink to="">e</NavLink>
-          <NavLink to="">r</NavLink>
+        <div 
+        className='flex gap-10 nv-link items-center p-5'>
+          <NavLink to="/" className='transition delay-150 duration-300 ease-in hover:-translate-y-1 hover:scale-130 hover:underline'>My Work</NavLink>
+          <NavLink to="/" className='transition delay-150 duration-300 ease-in hover:-translate-y-1 hover:scale-130 hover:underline'>Work Experience</NavLink>
+          <NavLink to="/" className='transition delay-150 duration-300 ease-in hover:-translate-y-1 hover:scale-130 hover:underline'>Education</NavLink>
+          <NavLink to="/" className='transition delay-150 duration-300 ease-in hover:-translate-y-1 hover:scale-130 hover:underline'>About Me</NavLink>
+          <NavLink to="/" className='transition delay-150 duration-300 ease-in hover:-translate-y-1 hover:scale-130 hover:underline'>Contact</NavLink>
         </div>
 
       </nav>
