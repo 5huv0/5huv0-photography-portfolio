da3d53b65fe7d94660b3aad930f5ed6fb7bc1963
5huv0 <md88748874@gmail.com>
2025-04-22 23:19:12 +0600

    added about section

--- pages/NavContact.jsx
+++ pages/NavContact.jsx
@@ -1,10 +1,54 @@
 import React from 'react'
+import {Link} from 'react-router-dom'
 
-export default function NavContact() {
+export default function Contacts() {
   return (
-    <div>
-      <div></div>
-      <div></div>
+    <div className='flex flex-col items-center justify-center h-screen'>
+
+      <div className='bg-[#FEE715] w-9/12 h-80 p-20 flex justify-center'> {/* This is text head part*/}
+        <p className='font-extrabold text-7xl flex font-serif text-[#101820]'>
+          Connect with me Here
+        </p>
+      </div>
+
+      <div className='bg-[#FEE715] w-9/12 p-10 flex justify-center gap-5 '> {/* This is linkings part*/}
+        <button className='hover:bg-red-600 rounded-full w-16 h-8 justify-center flex transition ease-in-out delay-100'>
+            <Link
+              to="https://github.com/5huv0"
+            >
+              <img src="/images/github-logo.png" alt="" />
+            </Link>
+          </button>
+          <button className='hover:bg-red-600 rounded-full w-16 h-8 justify-center flex transition ease-in-out delay-100'>
+            <Link
+              to="https://www.facebook.com/share/1DSDLmcNCB/"
+            >
+              <img src="/images/fb-logo.png" alt="" />
+            </Link>
+          </button>
+          <button className='hover:bg-red-600 rounded-full w-16 h-8 justify-center flex transition ease-in-out delay-100'>
+            <Link
+              to="https://www.instagram.com/5huv_0?igsh=ZDVuMm0wOTBtMnln"
+            >
+              <img src="/images/insta-logo.png" alt="" />
+            </Link>
+          </button>
+          <button className='hover:bg-red-600 rounded-full w-16 h-8 justify-center flex transition ease-in-out delay-100'>
+            <Link
+              to="https://x.com/MdShuvo76030551?t=EYDUmqMZbLgsENpAbjW4Kw&s=09"
+            >
+              <img src="/images/twitter-logo.png" alt="" />
+            </Link>
+          </button>
+          <button className='hover:bg-red-600 rounded-full w-16 h-8 justify-center flex transition ease-in-out delay-100'>
+            <Link
+              to="mailto: md88748874@gmail.com"
+            >
+              <img src="/images/gmail-logo.png" alt="" />
+            </Link>
+          </button>
+      </div>
+
     </div>
   )
 }
