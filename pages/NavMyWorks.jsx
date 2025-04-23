244cc43463f7f5b553b82b520302e12f3e6e558c
5huv0 <md88748874@gmail.com>
2025-04-24 02:18:53 +0600

    work section  done

--- pages/NavMyWorks.jsx
+++ pages/NavMyWorks.jsx
@@ -1,9 +1,15 @@
 import React from 'react'
+import SliderComp from '../src/components/home-components/my-works-section/slider/SliderComp'
 
 export default function NavMyWorks() {
   return (
     <div>
-      
+      <div className='bg-amber-900'>
+        <p className='font-bold text-8xl text-center p-5 m-5'>My works</p>
+      </div>
+      <div className='mb-20'>
+        <SliderComp/>
+      </div>
     </div>
   )
 }
