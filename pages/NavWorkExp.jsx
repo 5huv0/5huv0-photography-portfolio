ee97130aa559621e9beebb3aaf23252c04ff3084
5huv0 <md88748874@gmail.com>
2025-04-24 02:23:40 +0600

    work exp  section  done

--- pages/NavWorkExp.jsx
+++ pages/NavWorkExp.jsx
@@ -1,9 +1,10 @@
 import React from 'react'
+import WorksExp from '../src/components/home-components/my-work-exp-section/WorkExp'
 
 export default function NavWorkExp() {
   return (
     <div>
-      
+      <WorksExp />
     </div>
   )
 }
